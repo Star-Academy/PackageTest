a5bc4343542e49139d70004e6657dd09743411f9
ahmadsalimi <ahsa9978@gmail.com>
2020-08-02 17:09:57 +0430

    add publish again

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SampleLibrary;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adder = new Adder();
+
+            Console.WriteLine(adder.Add(2, 3));
+        }
+    }
+}
